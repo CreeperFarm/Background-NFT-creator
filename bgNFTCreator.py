9d8f387e202d1bee40eb13ac31f1750e13e9b6f1
CreeperFarm <creeperfarm.pro@gmail.com>
2022-04-04 12:58:43 +0200

    Update bgNFTCreator.py

--- bgNFTCreator.py
+++ bgNFTCreator.py
@@ -1,1 +1,8 @@
+from PIL import Image
 
+print("Which language do you use ? (Fr, En)") #Language chooser
+input(language)
+if language == "fr" or language == "French" or language == "Français":
+  print("")
+
+im = Image.new("RGB", (x, y), (255, 255, 255))
