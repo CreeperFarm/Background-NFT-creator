338304bb74629549b727a62aa64ba6c2acd14241
CreeperFarm <creeperfarm.pro@gmail.com>
2022-04-05 20:36:26 +0200

    Create smoothbgNFTCreator.py

---
+++ smoothbgNFTCreator.py
@@ -0,0 +1,83 @@
+from PIL import Image
+import random
+
+x_=0
+y_=0
+nb=0
+language="?"
+
+def img(x_, y_, nb):
+      im = Image.new("RGB", (x_, y_), (255, 255, 255))
+      
+      t=0
+      while t != nb:
+            r = int(random.randint(0, 255))
+            g = int(random.randint(0, 255))
+            b = int(random.randint(0, 255))
+            for x in range(0, x_):
+                  for y in range(0, x_):
+                        r = int(random.randint(r-10, r+10))
+                        g = int(random.randint(r-10, r+10))
+                        b = int(random.randint(r-10, r+10))
+                        im.putpixel((x, y), (r, g, b))
+            t=t+1
+            im.save("background " + str(t) + ".png")        
+
+def fr():
+      print("Quel largeur voulez-vous pour votre fond ?")
+      x_=int(input())
+      if x_ <= 0:
+            print("Nombre Invalid, entrer en un supérieur à 0.")
+            print("Pour entrer une valeur correct, redémarrer le programme.")
+            exit()
+      else:
+            print("C'est bon!")
+      print("Quel longueur voulez-vous pour votre fond ?")
+      y_=int(input())
+      if y_ <= 0:
+            print("Nombre Invalid, entrer en un supérieur à 0.")
+            print("Pour entrer une valeur correct, redémarrer le programme.")
+            exit()
+      else:
+            print("C'est bon!")
+      print("Combien de fond voulez-vous ?")
+      nb=int(input())
+      if nb <= 0:
+            print("Nombre Invalid, entrer en un supérieur à 0.")
+            print("Pour entrer une valeur correct, redémarrer le programme.")
+            exit()
+      else:
+            print("C'est bon!")
+      img(x_, y_, nb)
+
+def en():
+      print("Warning : Enter only positive ! ")
+      x_=int(input("What lenght do you want for your background (in pixels) ? "))
+      if x_ <= 0:
+            print("Invalid number, set one upper than 0. ")
+            print("To enter valid number restart the programme.")
+            exit()
+      else:
+            print("It's ok! ")
+      y_=int(input("What width do you want for your background (in pixels) ?  "))
+      if y_ <= 0:
+            print("Invalid number, set one upper than 0.")
+            print("To enter valid number restart the programme.  ")
+            exit()
+      else:
+            print("It's ok!")
+      nb=int(input("How many background do you want ?  "))
+      if nb <= 0:
+            print("Invalid number, set one upper than 0.")
+            print("To enter valid number restart the programme.")
+            exit()
+      else:
+            print("It's ok!")
+      img(x_, y_, nb)
+
+print("Which language do you use ? (Fr, En, more will be added soon...)") #Language chooser
+language=input()
+if language == "fr" or language == "French" or language == "Français":
+      fr()
+elif language == "en" or language == "English":
+      en()
